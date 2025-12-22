bbfbc90aca86fbdf2ef6600530d3308627f2a50f
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2025-12-22 13:12:21 -0400

    reatribuição de arrays e introdução ao Garbage Collector

--- src/Conditions/ArraysAula.java
+++ src/Conditions/ArraysAula.java
@@ -6,7 +6,7 @@ public class ArraysAula {
 
         /*boa prática, colchetes depois do tipo e sem espaço entre
         o colchete e o tipo*/
-        String[] ninja = new String[3];
+        String[] ninja = new String[5];
 
 
         /*Diferença entre Array e list é que o Array trabalha com essa alocação de memória, eu tenho um tipo
@@ -29,6 +29,33 @@ public class ArraysAula {
         /*double inicializa 0.0*/
         double[] pontoFlutuante = new double[1];
         System.out.println(pontoFlutuante[0]);
+
+        /*redeclarando variável*/
+        String hokageName = "Naruto";
+        hokageName = "Boruto";
+        System.out.println(hokageName);
+
+        /*redeclarando array(REATRIBUIÇÃO DE REFERÊNCIA), ele usa outro espaço de memória pra armazenar os novos itens
+        * e os itens antigos não existem mais, o resultado esperado é null agora que o array foi redeclarado*/
+        int qtdHokages = 7;
+        ninja = new String[qtdHokages];
+        ninja[0] = "Hashirama";
+        ninja[1] = "Tobirama";
+        ninja[2] = "Iruzen";
+        ninja[3] = "Minato";
+        ninja[4] = "Tsunade";
+        ninja[5] = "Kakashi";
+        ninja[6] = "Naruto";
+
+        /*O Garbage Collector vai ser o responsável por verificar o que está ocupando espaço na memória
+        * e não está mais sendo utilizado, nesse caso o nosso antigo array de ninjas, ele faz uma espécie
+        * de varredura*/
+
+        /*for pra passar por todo o meu array*/
+        for (int i = 0; i < 7; i++) {
+            System.out.println(ninja[i]);
+
+        }
 
 
     }
