023c58b1e17fd23eb8cbcb71b96b86524e621bb7
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2025-12-22 12:43:03 -0400

    finalizado parte básica de Arrays

--- src/Conditions/ArraysAula.java
+++ src/Conditions/ArraysAula.java
@@ -3,20 +3,34 @@ package Conditions;
 public class ArraysAula {
     public static void main(String[] args) {
 
-        String[] ninja = new String[3];/*boa prática, colchetes depois do tipo e sem espaço entre
+
+        /*boa prática, colchetes depois do tipo e sem espaço entre
         o colchete e o tipo*/
+        String[] ninja = new String[3];
+
+
+        /*Diferença entre Array e list é que o Array trabalha com essa alocação de memória, eu tenho um tipo
+        * definido para o Array e um limite de itens que vai ter no meu array
+        *
+        * cada array tem a sua REFERENCIA, é o que faz meu item pertencer a um determinado ARRAY
+        * Se gasta muito mais memória criando várias variáveis de ninjas do que com um array com 300 ninjas*/
         ninja[0] = "Naruto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
 
-        int[] numero = new int[3];
-        numero[0] = 12;
-        numero[1] = 22;
-        numero[2] = 17;
+        /*String inicializa com null*/
+        System.out.println(ninja[1]);
 
-        System.out.println(numero[0] + numero[2]);
+        /*new = new objeto de memória*/
+        /*Boolean incializa false*/
+        boolean[] verdadeiroOuFalso = new boolean[1];
+        System.out.println(verdadeiroOuFalso[0]);
 
-        System.out.println(ninja[0]);
+        /*double inicializa 0.0*/
+        double[] pontoFlutuante = new double[1];
+        System.out.println(pontoFlutuante[0]);
+
+
     }
 
 
