c78db17567fa74be9dda8c6db324f291fdfd8ada
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2025-12-31 13:27:23 -0400

    aplicado conceitos de métodos, atributos e parâmetros de Java. Nota: eu acabei utilizando Pascal case em alguns métodos o que não é recomendado, futuramente vou mudar tudo para camelCase do jeito certo, melhor forma que tem para se trabalhar em Java

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -8,9 +8,19 @@ public class Main {
         System.out.println("========== Sasuke ==========");
         Ninja sasuke= new Ninja();
 
+        //atributos do meu objeto
         sasuke.nome = "Sasuke Uchiha";
         sasuke.idade = 18;
         sasuke.aldeia = "Aldeia da Folha";
+
+       /* método aplicado no meu objeto sasuke, os () indicam pro java que isso é um método*/
+        /*sasuke.BoasVindas();  -->  Assim o método não aparece no terminal, porque está retornando
+        * somente um valor*/
+        String boasVindasSasuke = sasuke.BoasVindas();
+        System.out.println(boasVindasSasuke);
+        sasuke.sharinganAtivado();
+
+
 
         System.out.println(sasuke.nome + "\n" + sasuke.idade + "\n" + sasuke.aldeia);
 
@@ -23,6 +33,14 @@ public class Main {
         naruto.aldeia= "Aldeia da Folha";
         System.out.println(naruto.nome + "\n" + naruto.idade + "\n" + naruto.aldeia);
 
+        String boasVindasNaruto = naruto.BoasVindas();
+        int tempoRestanteHokage = naruto.AnosHogake(50);
+
+        System.out.println(boasVindasNaruto);
+        System.out.println("Faltam "+ tempoRestanteHokage + " anos para eu me tornar Hokage");
+
+
+
 
         System.out.println("========== Sakura ==========");
 
@@ -33,6 +51,9 @@ public class Main {
         sakura.idade = 17;
         sakura.aldeia= "Aldeia da Folha";
         System.out.println(sakura.nome + "\n" + sakura.idade + "\n" + sakura.aldeia);
+
+        String boasVindasSakura = sakura.BoasVindas();
+        System.out.println(boasVindasSakura);
 
 
 
--- src/Intermediario/Ninja.java
+++ src/Intermediario/Ninja.java
@@ -4,4 +4,23 @@ public class Ninja {
     String nome;
     String aldeia;
     int idade;
+
+    /*método publico personalizado, como eu não quero que ela me retorne algo
+    *    vai ser uma função VOID*/
+    public void sharinganAtivado(){
+        System.out.println("Sharingan Ativado!");
+
+    }
+
+    /*método String que retorna uma mensagem String de boas vindas*/
+    public String BoasVindas(){
+        return "Olá, eu sou o ninja "+nome + "!";
+    }
+
+    /*método pra retornar tipo INT, aqui eu vou passar um parâmetro para a minha função
+    *    que é a idade mínima para ser hokage*/
+    public int AnosHogake(int idadeMinima){
+        return idadeMinima - idade;
+
+    }
 }
