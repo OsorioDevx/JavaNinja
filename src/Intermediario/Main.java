72d617d16e52bd9b89e1f742bd557db660176b38
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2025-12-31 02:10:04 -0400

    criado main do nível intermediário, começando a ver sobre classes e objetos. Criei a classe Ninja para onde vou atribuir nome, idade e aldeia de cada ninja

--- src/Intermediario/Main.java
+++ src/Intermediario/Main.java
@@ -1,5 +1,42 @@
 package Intermediario;
 
 public class Main {
+    public static void main(String[] args) {
+        /*No trabalho você não vai ficar criando variáveis na Main, você
+        * trabalha com objetos!!*/
+        System.out.println("Inicializando Objetos");
+        System.out.println("========== Sasuke ==========");
+        Ninja sasuke= new Ninja();
+
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.idade = 18;
+        sasuke.aldeia = "Aldeia da Folha";
+
+        System.out.println(sasuke.nome + "\n" + sasuke.idade + "\n" + sasuke.aldeia);
+
+        System.out.println("========== Naruto ==========");
+
+        Ninja naruto = new Ninja();
+
+        naruto.nome= "Naruto Uzumaki";
+        naruto.idade = 16;
+        naruto.aldeia= "Aldeia da Folha";
+        System.out.println(naruto.nome + "\n" + naruto.idade + "\n" + naruto.aldeia);
+
+
+        System.out.println("========== Sakura ==========");
+
+        /*Ctrl + botão esquerdo leva direto para a classe*/
+        Ninja sakura = new Ninja();
+
+        sakura.nome= "Sakura Haruno";
+        sakura.idade = 17;
+        sakura.aldeia= "Aldeia da Folha";
+        System.out.println(sakura.nome + "\n" + sakura.idade + "\n" + sakura.aldeia);
+
+
+
+
+    }
 
 }
