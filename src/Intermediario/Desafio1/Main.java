738de9f13968de64b12c74c80ad8ad3e00431496
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2026-01-02 14:31:24 -0400

    finalizado desafio 3 para fixar os conhecimentos de Herança

---
+++ src/Intermediario/Desafio1/Main.java
@@ -0,0 +1,22 @@
+package Intermediario.Desafio1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Programa de Gerenciamento de Ninjas da Folha");
+
+
+        System.out.println("====================");
+        Uchiha sasuke = new Uchiha();
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.idade = 18;
+        sasuke.missao = "Derrotar Orochimaru";
+        sasuke.nivelDificuldade = "Rank S";
+        sasuke.statusMissao = "Em andamento";
+        sasuke.habilidadeEspecial = "Sharingan";
+
+        sasuke.mostrarInformacoes();
+
+
+
+    }
+}
---
+++ src/Intermediario/Desafio1/Uchiha.java
@@ -0,0 +1,15 @@
+package Intermediario.Desafio1;
+
+public class Uchiha extends Ninja {
+    String habilidadeEspecial;
+
+    public void mostrarInformacoes() {
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("Missão: " + missao);
+            System.out.println("Rank da Missão: " + nivelDificuldade);
+            System.out.println("Status da Missão: " + statusMissao);
+            System.out.println("Habilidade Especial: " + habilidadeEspecial);
+        }
+    }
+
