6f2ceb1fecdcf3e73dc8dc25d22f63c323e20c6e
Matheus Henrique Gama Osório <henrique.busn@gmail.com>
2026-01-24 00:41:46 -0400

    trabalhando com constructors, criei um package só de constructors, aplicando o conceito de this

---
+++ src/Construtores/Hokage.java
@@ -0,0 +1,46 @@
+package Construtores;
+
+public class Hokage {
+    String nome;
+    int ordem;
+    boolean vivoOuNao;
+    String clan;
+    int missoes;
+    double saldoBancario;
+    double altura;
+
+
+    /*construtor com argumento (nome)*/
+    public Hokage(String nome) {
+        this.nome = nome;
+    }
+
+    public Hokage(int ordem) {
+        this.ordem = ordem;
+
+    }
+
+    /*All args constructor = construtor com todos os argumentos*/
+    public Hokage(String nome, int ordem, boolean vivoOuNao){
+        this.nome = nome;
+        this.ordem = ordem;
+        this.vivoOuNao = vivoOuNao;
+    }
+
+    /*usando alt + insert para criar todos os construtores automaticamente*/
+
+    public Hokage(String nome, int ordem, boolean vivoOuNao, String clan, int missoes, double saldoBancario, double altura) {
+        this.nome = nome;
+        this.ordem = ordem;
+        this.vivoOuNao = vivoOuNao;
+        this.clan = clan;
+        this.missoes = missoes;
+        this.saldoBancario = saldoBancario;
+        this.altura = altura;
+    }
+
+    /*O padrão que se utiliza é somente 1 construtor com todos os argumentos e 1 construtor vazio */
+}
+
+
+
---
+++ src/Construtores/Main.java
@@ -0,0 +1,24 @@
+package Construtores;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Hokage hashirama = new Hokage("Hashirama Senju");
+        hashirama.ordem = 1;
+        hashirama.vivoOuNao= false;
+
+        Hokage tobirama = new Hokage("Tobirama Senju");
+        System.out.println(tobirama.nome);
+
+        Hokage hiruzen = new Hokage(3);
+        System.out.println(hiruzen.ordem);
+
+        Hokage minato = new Hokage("Minato Namikaze",4,false);
+        System.out.println();
+
+        Hokage tsunade = new Hokage("Tsunade", 5,true, "Senju", 300, 50000.00, 1.70);
+
+
+
+    }
+}
